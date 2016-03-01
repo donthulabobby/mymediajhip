72dfc35d77978d409a92378358358dd71dbac06c
donthulabobby <bobbydonthula@gmail.com>
2016-03-01 16:05:25 -0500

    added video entity

---
+++ src/main/webapp/scripts/app/entities/video/video.controller.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('mymediajhipApp')
+    .controller('VideoController', function ($scope, $state, Video, VideoSearch) {
+
+        $scope.videos = [];
+        $scope.loadAll = function() {
+            Video.query(function(result) {
+               $scope.videos = result;
+            });
+        };
+        $scope.loadAll();
+
+
+        $scope.search = function () {
+            VideoSearch.query({query: $scope.searchQuery}, function(result) {
+                $scope.videos = result;
+            }, function(response) {
+                if(response.status === 404) {
+                    $scope.loadAll();
+                }
+            });
+        };
+
+        $scope.refresh = function () {
+            $scope.loadAll();
+            $scope.clear();
+        };
+
+        $scope.clear = function () {
+            $scope.video = {
+                video_name: null,
+                added_by: null,
+                language: null,
+                video_location: null,
+                id: null
+            };
+        };
+    });
---
+++ src/main/webapp/scripts/app/entities/video/video.js
@@ -0,0 +1,125 @@
+'use strict';
+
+angular.module('mymediajhipApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('video', {
+                parent: 'entity',
+                url: '/videos',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'mymediajhipApp.video.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/video/videos.html',
+                        controller: 'VideoController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('video');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+            .state('video.detail', {
+                parent: 'entity',
+                url: '/video/{id}',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'mymediajhipApp.video.detail.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/video/video-detail.html',
+                        controller: 'VideoDetailController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('video');
+                        return $translate.refresh();
+                    }],
+                    entity: ['$stateParams', 'Video', function($stateParams, Video) {
+                        return Video.get({id : $stateParams.id});
+                    }]
+                }
+            })
+            .state('video.new', {
+                parent: 'video',
+                url: '/new',
+                data: {
+                    authorities: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/video/video-dialog.html',
+                        controller: 'VideoDialogController',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {
+                                    video_name: null,
+                                    added_by: null,
+                                    language: null,
+                                    video_location: null,
+                                    id: null
+                                };
+                            }
+                        }
+                    }).result.then(function(result) {
+                        $state.go('video', null, { reload: true });
+                    }, function() {
+                        $state.go('video');
+                    })
+                }]
+            })
+            .state('video.edit', {
+                parent: 'video',
+                url: '/{id}/edit',
+                data: {
+                    authorities: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/video/video-dialog.html',
+                        controller: 'VideoDialogController',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Video', function(Video) {
+                                return Video.get({id : $stateParams.id});
+                            }]
+                        }
+                    }).result.then(function(result) {
+                        $state.go('video', null, { reload: true });
+                    }, function() {
+                        $state.go('^');
+                    })
+                }]
+            })
+            .state('video.delete', {
+                parent: 'video',
+                url: '/{id}/delete',
+                data: {
+                    authorities: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/video/video-delete-dialog.html',
+                        controller: 'VideoDeleteController',
+                        size: 'md',
+                        resolve: {
+                            entity: ['Video', function(Video) {
+                                return Video.get({id : $stateParams.id});
+                            }]
+                        }
+                    }).result.then(function(result) {
+                        $state.go('video', null, { reload: true });
+                    }, function() {
+                        $state.go('^');
+                    })
+                }]
+            });
+    });
