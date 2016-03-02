c7e808b966c09950b27079124e6a1890f5bce730
donthulabobby <bobbydonthula@gmail.com>
2016-03-01 19:18:40 -0500

    Revert "added video entity"
    This reverts commit 72dfc35d77978d409a92378358358dd71dbac06c.

--- src/main/webapp/scripts/components/entities/video/video.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('mymediajhipApp')
-    .factory('VideoSearch', function ($resource) {
-        return $resource('api/_search/videos/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
